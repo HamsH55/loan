cd91bff354a9a8f6c19ffc592a14846c0a223a3d
HamsH55 <hams@ditech.sa>
2024-04-08 11:45:38 +0300

    kk

---
+++ src/AnotherPage.js
@@ -0,0 +1,11 @@
+
+
+export default function AnotherPage(){
+    return(
+        <div>
+        
+        <h1>Hello this is Another Page </h1>
+        
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -6,10 +6,29 @@ import './App.css';
 //import Typography from "@mui/material/Typography";
 import LoanForm from './LoanForm';
 import { UserContext } from './context/UserContext';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Typography from '@mui/material/Typography';
+
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import AnotherPage from './AnotherPage';
+import { createTheme , ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme ({
+
+typography:{
+  fontFamily:["Rubik"]
+}
+
+})
 
 function App() {
 
   return (
+    // <ThemeProvider theme={theme}>
+
+    
     <UserContext.Provider value={{
       userName:"@HAMS",
       name:"Hams" ,
@@ -23,8 +42,22 @@ function App() {
     color: "white",
   }}>   
         <LoanForm />
+        {/* <Typography variant="h4" gutterBottom>
+        h4. Heading
+      </Typography> */}
+
+        <Router>
+  <Routes>
+
+<Route path='/AnotherPage' element ={<AnotherPage/>}>
+
+</Route>
+  </Routes>
+</Router>
+
     </div>
     </UserContext.Provider>
+
   );
 }
 
